68ef8fef5cc02151647a83ecf4690699c6ab3d0f
Albert Escamilla <albert.esc@gmail.com>
2021-12-23 19:55:54 +0100

    feat: clean indexhtml and add packages on main.js

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
-import accordion from '@choostdev/accordion'
-import tabs from '@choostdev/tabs'
+import './style.css'
+import accordion from './packages/accordion'
+import dropdown from './packages/dropdown'
+import select from './packages/select'
+import tabs from './packages/tabs'
 
-tabs({ tab: 'tab', panel: 'tab-panel' })
-accordion({ button: 'accordion-button', panel: 'accordion-panel' })
+accordion({
+  button: 'accordion-button',
+  panel: 'accordion-panel'
+})
+
+dropdown({
+  dropdown: 'dropdown',
+  menu: 'dropdown-menu'
+})
+
+select({
+  select: 'select',
+  selectBox: 'select-box',
+  selectOption: 'select-option'
+})
+
+tabs({
+  tab: 'tab',
+  panel: 'tab-panel'
+})
